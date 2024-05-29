5a45a25b370860ec6582414ccc371271f9c380f7
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2024-05-29 19:11:35 +0530

    update containerization

---
+++ init.py
@@ -0,0 +1,7 @@
+import os
+os.system("pip3 install flask")
+os.system("pip3 install pyjwt")
+os.system("pip3 install pymongo")
+os.system("pip3 install mysql-connector-python")
+os.system("pip3 install python-dotenv")
+os.system("pip3 install bcrypt")
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
-os.system("pip3 install flask")
-os.system("pip3 install pyjwt")
-os.system("pip3 install pymongo")
-os.system("pip3 install mysql-connector-python")
-os.system("pip3 install python-dotenv")
-os.system("pip3 install bcrypt")
+# os.system("pip3 install flask")
+# os.system("pip3 install pyjwt")
+# os.system("pip3 install pymongo")
+# os.system("pip3 install mysql-connector-python")
+# os.system("pip3 install python-dotenv")
+# os.system("pip3 install bcrypt")
 from flask import Flask, request, jsonify, make_response, request, render_template, session, flash, redirect
 import jwt
 from pymongo import MongoClient
