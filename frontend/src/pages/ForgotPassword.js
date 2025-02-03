e5dafb6099141bb8d63b11ef9e5e3b5abf7e0c16
Sanjiv Kannaa Jeganathan <jsanjiv2003@gmail.com>
2025-02-03 11:21:43 +0530

    added forgotPassword.js

---
+++ frontend/src/pages/ForgotPassword.js
@@ -0,0 +1,13 @@
+import './css/global.css';
+
+function ForgotPassword() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Forgot Password
+      </header>
+    </div>
+  );
+}
+
+export default ForgotPassword;
