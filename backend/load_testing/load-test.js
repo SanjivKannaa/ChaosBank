9d22c896ae7d43358465107af31dbd636b04141c
Sanjiv Kannaa Jeganathan <jsanjiv2003@gmail.com>
2025-03-24 12:37:52 +0530

    updated load test and postman collection

--- backend/load_testing/load-test.js
+++ backend/load_testing/load-test.js
@@ -7,6 +7,40 @@ export let options = {
     duration: '15m', // Test duration
 };
 
+function generateRandomPhoneNumber() {
+    return Math.floor(1000000000 + Math.random() * 9000000000).toString();
+}
+
+function generateAddress() {
+    return {
+        door_plot_no: "123",
+        street_name_1: "Main Street",
+        street_name_2: "Apt 456",
+        city: "Pune",
+        state: "Maharashtra",
+        country: "India",
+        pin: generateRandomPinCode()
+    };
+}
+
+function generateRandomAadhar() {
+    return Math.floor(100000000000 + Math.random() * 900000000000).toString();
+}
+
+function generateRandomPan() {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const digits = '0123456789';
+    return (
+        Array.from({ length: 5 }, () => letters[Math.floor(Math.random() * letters.length)]).join('') +
+        Array.from({ length: 4 }, () => digits[Math.floor(Math.random() * digits.length)]).join('') +
+        letters[Math.floor(Math.random() * letters.length)]
+    );
+}
+
+function generateRandomPinCode() {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+}
+
 export default function () {
     let username = `load_test_user_${uuidv4()}`;
     let password = 'TestTest@1234';
@@ -16,8 +50,12 @@ export default function () {
         username: username,
         profileName: username,
         password: password,
-        email: username+"@mail.com",
-        phoneNumber: Math.floor(1000000000 + Math.random() * 9000000000).toString()
+        email: `${username}@mail.com`,
+        phoneNumber: generateRandomPhoneNumber(),
+        date_of_birth: "1995-05-15",
+        aadhar_number: generateRandomAadhar(),
+        pan_number: generateRandomPan(),
+        address: generateAddress()
     });
     let registerRes = http.post('http://api.chaosbank.sanjivkannaa.tech/auth/register', registerPayload, {
         headers: { 'Content-Type': 'application/json' }
